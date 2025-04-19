dc6a7062782f2e81bb8e41338ac17d99b2ab9541
AdisheshBalaji <adishesh@aether.>
2025-04-19 00:03:27 -0700

    Fixed the firebase version

--- server/index.js
+++ server/index.js
@@ -2,50 +2,48 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
-const {Server} = require("socket.io");
-app.use(cors());
+const { Server } = require("socket.io");
+// const chat = require('./chat'); // Uncomment this if you need chat.js
+// const { initializeApp } = require('firebase/app'); // Not used currently
+// const { getFirestore } = require('firebase/firestore'); // Not used currently
 
+app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors : {
+    cors: {
         origin: "*",
-        // methods: ["GET", "POST"]
     },
 });
 
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
-    socket.on('join_room', (data) =>{
+    socket.on('join_room', (data) => {
         socket.join(data);
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    })
+    });
 
-    socket.on('send_message', (data) =>{
-        socket.to(data.room).emit('receive_message',data);
-    })
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('receive_message', data);
+    });
 
-    socket.on('disconnect', () =>{
-        console.log('User disconnected', socket.id);
-    })
     socket.on('typing', ({ room, username }) => {
         socket.to(room).emit('typing', { username });
-      });
-    console.log(`${username} is typing in room ${room}`);
+        console.log(`${username} is typing in room ${room}`);
+    });
+
     socket.on('stop_typing', ({ room, username }) => {
         socket.to(room).emit('stop_typing', { username });
-      });
-      console.log(`${username} stopped typing in room ${room}`);
-      
+        console.log(`${username} stopped typing in room ${room}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected', socket.id);
+    });
 });
 
-
-
-
-
-server.listen(3001, () => {
-    console.log('Server running')
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
- 
